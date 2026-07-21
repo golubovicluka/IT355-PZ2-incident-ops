c32e2f432367e802b4a2781233a1be7636aeaf70
Luka Golubovic <luka_golubovic@yahoo.com>
2026-07-22 01:29:49 +0200

    Initial commit

---
+++ src/main/java/com/golubovicluka/incident_ops/IncidentOpsApplication.java
@@ -0,0 +1,13 @@
+package com.golubovicluka.incident_ops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentOpsApplication.class, args);
+	}
+
+}
